08ae05e174a5b493bbd11e1410b87b1afb2b33ed
ELITE-E <kskroyal@arch.localdomain>
2025-11-14 17:46:57 +0300

    fix detail errors

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
 
 from . import views
+from products import urls
 #from .views import api_home/roughtly the same as above hehehe
 
 urlpatterns=[
     path('',views.api_home),
     #path('products/',include(products.urls))--->Another method of including the api endpoit
+    #path('products/',include(products.urls))
 ]
--- backend/cfehome/urls.py
+++ backend/cfehome/urls.py
@@ -5,6 +5,14 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
+endpoint ="http://127.0.0.1:8000/api/products/1/"
+get_res=requests.get(endpoint)
+
+print(f"Response Status:{get_res.status_code}")
+#print(f"Res text:{get_res.text}")
+
+#print(f"{get_res.__format__}")
+#print(get_res.json())
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
@@ -20,5 +28,5 @@ from django.urls import path,include#this includes is a must
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
-    path('api/products',include('products.urls'))
+    path('api/',include('products.urls'))
 ]
