1e46b2d95f31c6f3d08a6de4857004787b508faa
ELITE-E <kskroyal@arch.localdomain>
2025-11-17 23:47:57 +0300

    Impelement multiple products generic views

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,13 +4,29 @@ from .serializers import ProductSerializer
 from .models import Product
 
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    """
+    A combination of List + crete api view 
+    """
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+
+product_list_create_view=ProductListCreateAPIView.as_view()
+
 class  ProductDetailAPIView(generics.RetrieveAPIView):
 
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
 
 
-#product_detail_view=ProductDetailAPIView.as_view()
+product_detail_view=ProductDetailAPIView.as_view()
     #for specific fields you pass the/it's  pk (primarykey)
+class  ProductListAPIView(generics.ListAPIView):
+    """
+    But we wont use it 
+    """
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
 
-# Create your views here.
+product_list_view=ProductListAPIView.as_view()
